6150b840d7ba0b4a5c8f654ad9d03dcf32e55bc6
KGFAT <32797449+killgfat@users.noreply.github.com>
2023-08-24 19:14:53 +0800

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -12,8 +12,6 @@ chmod +x ./mcl-installer
 mkdir app && cd app
 # 安装临时的 fix-protocol-version 插件
 mkdir plugins
-wget https://github.com/cssxsh/fix-protocol-version/releases/download/v1.3.0/fix-protocol-version-1.3.0.mirai2.jar -O plugins/fix-protocol-version-1.3.0.mirai2.jar
-wget https://github.com/MrXiaoM/mirai-console-dev-qrlogin/releases/download/0.1.1/mirai-console-dev-qrlogin-0.1.1.jar -O plugins/mirai-console-dev-qrlogin-0.1.1.jar
 
 "../mcl-installer" << EOF
 N

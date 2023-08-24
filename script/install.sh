461233d9f6239f7914f13a85b81045002005832a
KGFAT <32797449+killgfat@users.noreply.github.com>
2023-08-24 19:27:52 +0800

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MCL_VERSION=ce4ab47
+MCL_VERSION=`wget -qO- -t1 -T2 "https://api.github.com/repos/iTXTech/mcl-installer/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g'`
 # Exit if any error occured
 set -euxo pipefail
 
